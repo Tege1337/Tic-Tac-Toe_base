1682549401f0bbf5ac129f1e58c11a0be6363e90
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-03-24 09:45:04 +0100

    update

--- tiktokto.py
+++ tiktokto.py
@@ -11,10 +11,10 @@ for i in range(3):
 
 #Pálya kiírása
 def print_grid():
-    for x in range(3):
+    for i in range(3):
         print('|', end="")
-        for y in range(3):
-            print(oszlop[x][y], "|", end="")
+        for j in range(3):
+            print(oszlop[i][j], "|", end="")
             print()
             
 
@@ -28,16 +28,44 @@ def player_turn(turn_player1):
         print(f"{player1} a soros")
     return turn_player1
 
+
 #hely kiválasztása
 def write_cell(cell):
     cell -= 1
     i = int(cell / 3)
-    j = cell % 3
+    j =  cell % 3   
     if turn_player1 == True:
-        grid[]
+        oszlop[i][j] = player1_symbol
+    else:
+        oszlop[i][j] = player2_symbol
+    return oszlop
+
+
+#megnézi hoogy szabad e a hely
+def free_cell(cell):
+    cell -= 1
+    i = int(cell / 3)
+    j =  cell % 3
+    if oszlop[i][j] == player1_symbol or groslopid[i][j] == player2_symbol:
+        print("This cell is not free")
+        return False
+    return True
 
 
 print("TIC-TAC-TOE")
 player1 = input("Kérlek add meg az első játékos nevét! ")
-player1_symbool = input("Kérlek add meg az első játékos szimbólumát! ")
+player1_symbol = input("Kérlek add meg az első játékos szimbólumát! ")
 player2 = input("Kérlek add meg a második játékos nevét! ")
+player2_symbol = input("Kérlek add meg a második játékos szimbólumát! ")
+
+game = True
+full_grid = False
+turn_player1 = False
+winner = ""
+
+
+while game == True:
+    turn_player1 = player_turn(turn_player1)
+    free_box = False
+    while free_box == False:
+        cell = int(inu
