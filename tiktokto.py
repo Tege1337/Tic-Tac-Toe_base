62153a85a0e74f13e2d8f946ff1508ce0a2a321d
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-03-24 09:34:06 +0100

    create tiktokto.py

---
+++ tiktokto.py
@@ -0,0 +1,24 @@
+#Pálya létrehozása
+oszlop = []
+sor = []
+
+for i in range(3):
+    for j in range(3):
+        sor.append(" ")
+    oszlop.append(sor)
+    sor = []
+         
+
+#Pálya kiírása
+def print_grid():
+    for x in range(3):
+        print('|', end="")
+        for y in range(3):
+            print(grid[x][y], "|", end="")
+            print()
+            
+
+#Játékos
+def player_turn(turn_player1):
+    if turn_player1 == True:
+        
