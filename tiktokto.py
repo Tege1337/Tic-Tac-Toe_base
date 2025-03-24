6437fec7ee00e724c2809ebdb766e38430226895
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-03-24 09:36:26 +0100

    print + rename

--- tiktokto.py
+++ tiktokto.py
@@ -14,7 +14,7 @@ def print_grid():
     for x in range(3):
         print('|', end="")
         for y in range(3):
-            print(grid[x][y], "|", end="")
+            print(oszlop[x][y], "|", end="")
             print()
             
 
@@ -22,6 +22,13 @@ def print_grid():
 def player_turn(turn_player1):
     if turn_player1 == True:
         turn_player1 = False
+        print(f"{player2} a soros")
+    else:
+        turn_player1 = True
+        print(f"{player1} a soros")
+    return turn_player1
 
-
-def write_cell(cell)
+#hely kiválasztása
+def write_cell(cell):
+    cell -= 1
+    i = int()
